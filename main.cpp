7abda20b772e807ab7050d375e648ae37ff07149
tlsdlstjq <94732614+tlsdlstjq@users.noreply.github.com>
2021-11-20 14:45:50 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+int main(void)
+{
+	//srand(time(NULL));
+	int a = rand() % 1000;
+	//int b = rand() % 1000;
+	//int c = rand() % 1000;
+	printf("%d %d %d", a, b, c);
+    return 0;
+}
